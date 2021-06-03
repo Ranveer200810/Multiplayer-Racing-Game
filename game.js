a7ad5de14e2a8f588664f93e35c03932a33a4309
Ranveer200810 <ranveerniras10@gmail.com>
2021-06-03 17:43:06 +0530

    RV : added Images of cars and track

--- game.js
+++ game.js
@@ -46,9 +46,16 @@ class Game {
         }
 
         car1 = createSprite(100, 200);
+        car1.addImage(car1Img);
+
         car2 = createSprite(300, 200);
+        car2.addImage(car2Img);
+
         car3 = createSprite(500, 200);
+        car3.addImage(car3Img);
+
         car4 = createSprite(700, 200);
+        car4.addImage(car4Img);
 
         cars = [car1, car2, car3, car4];
 
@@ -62,8 +69,11 @@ class Game {
 
         if (allPlayers !== undefined) {
 
+            background(ground);
+            image(track, 0, -displayHeight * 4, displayWidth, displayHeight * 5);
+
             var index = 0;
-            var x = 0;
+            var x = 175;
             var y;
 
             for (var plr in allPlayers) {
@@ -95,9 +105,19 @@ class Game {
 
         }
 
+        if (player.distance > 3860) {
+
+            gameState = 2;
+
+        }
+
         drawSprites();
 
-        
+    }
+
+    end() {
+
+        alert("Game Over");
 
     }
 
--- sketch.js
+++ sketch.js
@@ -2,14 +2,28 @@ var database;
 var form, playerCnt, player, game;
 var gameState = 0;
 var car1, car2, car3, car4;
+var car1Img, car2Img, car3Img, car4Img, track, ground;
 
 var allPlayers;
 var cars = [];
 
+function preload() {
+
+  car1Img = loadImage("car1.png");
+  car2Img = loadImage("car2.png");
+  car3Img = loadImage("car3.png");
+  car4Img = loadImage("car4.png");
+
+  ground = loadImage("ground.png");
+
+  track = loadImage("track.jpg");
+
+}
+
 function setup(){
 
   database = firebase.database();
-  createCanvas(displayWidth - 20, displayHeight - 30);
+  createCanvas(displayWidth + 20, displayHeight + 30);
 
   game = new Game();
   game.getGameState();
@@ -29,6 +43,12 @@ function draw(){
 
     clear();
     game.play();
+
+  }
+
+  if (gameState === 2) {
+
+    game.end();
 
   }
 
